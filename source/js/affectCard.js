63f5bc998221466dc17a33ae5813c62e173951b6
Eyespot <eger_np_@inbox.ru>
2018-03-22 19:30:38 +0300

    Завершает перестроения на разных ширинах

--- source/js/affectCard.js
+++ source/js/affectCard.js
@@ -80,6 +80,27 @@
     }());
   }
 
+  if (!Element.prototype.closest) {
+
+    Element.prototype.closest = function(css) {
+      var node = this;
+
+      while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+
+  if (!Element.prototype.matches) {
+
+    Element.prototype.matches = Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector;
+  }
+
   var CARD_CLASS = '.food-card';
   var DEFAULT_CARD_CLASS = 'food-card--default';
   var SELECTED_CARD_CLASS = 'food-card--selected';
